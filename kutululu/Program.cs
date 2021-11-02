dac57ffba30cd3f66047dc3147336a506a0a1d96
Aleš Janeček <Janecek.Ales@stud.vassboskovice.cz>
2021-11-02 08:42:39 +0100

    Volba

--- kutululu/Program.cs
+++ kutululu/Program.cs
@@ -6,7 +6,18 @@ namespace kutululu
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int volba
+            Console.WriteLine("kruh");
+            Console.WriteLine("=====================");
+            Console.WriteLine("1 -  poloměr ");
+            Console.WriteLine("2 -  průměr  ");
+            volba = int.Parse(Console.ReadLine());
+            Console.Write("Zadejte poloměr:   ");
+            double polomer = double.Parse(Console.ReadLine());
+            var obsah = 2 * 3.14 * polomer;
+            Console.Write            
+
+
         }
     }
 }
